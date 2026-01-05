aa3e421129365e10ead5c72571dc5c46334a3d96
_White Fox <oppovier@gmail.com>
2026-01-05 13:48:31 +0200

    Some fixes

--- src/main/java/foxiwhitee/FoxLib/tile/event/TileEventHandler.java
+++ src/main/java/foxiwhitee/FoxLib/tile/event/TileEventHandler.java
@@ -11,15 +11,9 @@ import java.lang.reflect.Method;
 
 public final class TileEventHandler {
     private final Method method;
-    private FoxBaseTile tile;
-
-    public TileEventHandler(Method method, FoxBaseTile tile) {
-        this.method = method;
-        this.tile = tile;
-    }
 
     public TileEventHandler(Method method) {
-        this(method, null);
+        this.method = method;
     }
 
     public void tick(FoxBaseTile tile) {
@@ -30,32 +24,12 @@ public final class TileEventHandler {
         }
     }
 
-    public void tick() {
-        tick(tile);
-    }
-
-    public TickRateModulation tickSpeed(FoxBaseTile tile) {
-        try {
-            return (TickRateModulation)this.method.invoke(tile);
-        } catch (InvocationTargetException | IllegalArgumentException | IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    public TickRateModulation tickSpeed()  {
-        return tickSpeed(tile);
-    }
-
     public void writeToNBT(FoxBaseTile tile, NBTTagCompound data) {
         try {
             this.method.invoke(tile, data);
         } catch (InvocationTargetException | IllegalArgumentException | IllegalAccessException e) {
             throw new IllegalStateException(e);
         }
-    }
-
-    public void writeToNBT(NBTTagCompound data) {
-        this.writeToNBT(tile, data);
     }
 
     public void readFromNBT(FoxBaseTile tile, NBTTagCompound data) {
@@ -66,20 +40,12 @@ public final class TileEventHandler {
         }
     }
 
-    public void readFromNBT(NBTTagCompound data) {
-        readFromNBT(tile, data);
-    }
-
     public void writeToStream(FoxBaseTile tile, ByteBuf data) {
         try {
             this.method.invoke(tile, data);
         } catch (InvocationTargetException | IllegalArgumentException | IllegalAccessException e) {
             throw new IllegalStateException(e);
         }
-    }
-
-    public void writeToStream(ByteBuf data) {
-        writeToStream(tile, data);
     }
 
     @SideOnly(Side.CLIENT)
@@ -89,14 +55,5 @@ public final class TileEventHandler {
         } catch (InvocationTargetException | IllegalArgumentException | IllegalAccessException e) {
             throw new IllegalStateException(e);
         }
-    }
-
-    @SideOnly(Side.CLIENT)
-    public boolean readFromStream(ByteBuf data) {
-        return readFromStream(tile, data);
-    }
-
-    Method getMethod() {
-        return method;
     }
 }
