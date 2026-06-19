e989f5d48d1ebc5af33261326c463b430ae4eff4
sasha <basashko@gmail.com>
2026-06-19 12:38:20 +0300

    fix: ukr font + mixins in dev + real environment. + fix crash with angelica

--- src/main/java/foxiwhitee/FoxLib/mixins/minecraft/FontRendererMixin.java
+++ src/main/java/foxiwhitee/FoxLib/mixins/minecraft/FontRendererMixin.java
@@ -1,22 +1,48 @@
 package foxiwhitee.FoxLib.mixins.minecraft;
 
 import net.minecraft.client.gui.FontRenderer;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = FontRenderer.class, priority = 1100)
+@Mixin(FontRenderer.class)
 public abstract class FontRendererMixin {
 
+    // TODO: fix font image with angelica
+    @Dynamic
     @ModifyConstant(
+        // TODO: fuck reobf in dev or real minecraft environment. here only 3 methods
         method = {
+            "func_78278_a",
+            "func_78263_a",
+            "func_78255_a",
             "renderCharAtPos",
             "renderStringAtPos",
             "getCharWidth"
         },
+        require = 0,
+        remap = false,
         constant = @Constant(stringValue = "ÀÁÂÈÊËÍÓÔÕÚßãõğİıŒœŞşŴŵžȇ\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑªº¿®¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αβΓπΣσμτΦΘΩδ∞∅∈∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■\u0000")
     )
     private String redirectFontCharacters(String original) {
-        return "ÀÁÂÈÊËÍÓÔÕÚßãÇüİıŒœŞşŴŵžȇ\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_õabcdefghijklmnopqrstuvwxyz{|}~ğҐґéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑªº¿®¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЇІЬЄЮЯабвгдежзийклмнопрстуфхцчшщїіьєюя";
+        return
+            "\u0000☺☻♥♦♣♠•◘○◙♂♀♪♫☼" +
+                "►◄↕‼¶§▬↨↑↓→←∟↔▲▼" +
+                " !\"#$%&'()*+,-./" +
+                "0123456789:;<=>?" +
+                "@ABCDEFGHIJKLMNO" +
+                "PQRSTUVWXYZ[\\]^_" +
+                "`abcdefghijklmno" +
+                "pqrstuvwxyz{|}~⌂" +
+                "АБВГДЕЖЗИЙКЛМНОП" +
+                "РСТУФХЦЧШЩҐІЬЄЮЯ" +
+                "абвгдежзийклмноп" +
+                "рстуфхцчшщґіьєюя" +
+                "Її\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u2116■" +
+                "░▒▓│┤╡╢╖╕╣║╗╝╜╛┐" +
+                "└┴┬├─┼╞╟╚╔╩╦╠═╬╧" +
+                "╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀"
+            ;
     }
 }
